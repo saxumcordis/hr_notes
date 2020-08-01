462b809b98606dd22675f5649240475927d19308
saxumcordis <victor123952@gmail.com>
2020-08-01 12:25:30 +0300

    Init first components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch, BrowserRouter} from "react-router-dom"
+import {Feedback} from "./Components/Feedback";
+import {Notes} from "./Components/NotesView";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Notes/>
+            <Route exact path="/feedback/:id">
+              <Feedback/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
   );
-}
+};
 
 export default App;
