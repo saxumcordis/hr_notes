f1bb72a3f40deb86fc61840b26d939bf45cb3cdd
saxumcordis <victor123952@gmail.com>
2020-08-02 08:14:32 +0300

    add tree

--- src/Components/Feedback.js
+++ src/Components/Feedback.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
+import {ProgressBar} from "./ProgressBar";
+import {Link} from "react-router-dom";
 
 const activeTool = {
     'border-bottom': "3px solid rgba(214,224,62, 0.9)"
 }
 
 const Feedback = () => {
+    const note =  {
+        id: 1,
+        header: "Create MVP",
+        deadline: "31.07.20",
+    };
 
     const content = [<ul>
         <li>Quality of product: 3/4</li>
@@ -20,10 +27,34 @@ const Feedback = () => {
     const [activeContent, setContent] = useState(0);
     const handleContent = content => setContent(content);
 
+    const [treeStage, setTreeStage] = useState(0);
+
+    const trees = ['https://psv4.userapi.com/c856224/u89830763/docs/d4/c3359f3c8af2/tree_1.png?extra=Qc4Yuiz1rfuVcAMOE8AMEVwpSbaWCwwYbbYY6kuOHbet147ghbskpgxOtRhJvqEHq--VGhH4VSfu75LRuz4PrZfJGj5ENg7Fi68gVYsYjid5CZS478xXFjo8dXgm2Rc3p9-qEJdTRFmlJxLWweja',
+        'https://psv4.userapi.com/c856224/u89830763/docs/d7/9247d7c03579/tree_2.png?extra=JqRJo0W0F4bnONITgDxMp3HoF4lxdtISNVatethaBUSPY3kqFXok8hXXYNbgRq1hpqoGjEH25yyyEJgOwdWyKv5ycXoqKUIMGqhdpdYUv1MzXLo3i8LWdwoVQ50eaaJYnZYaRYNlKcEZnW5urkCg',
+    'https://psv4.userapi.com/c856224/u89830763/docs/d2/efb2c2553885/tree_3.png?extra=DgvjJIpGa1Bem5NyflaJLVXyUWH3ew--aDaGjZJdc2o0S_32anx28XZHx_TQNTcFdojsneEkCqAGUIqcoED91DfHFhe65TNTd2ZuQ0Srd7cxjC2DL5WkQWNQqg3mrV_Tmr2NgYnR_AsfBB480KHn',
+        'https://psv4.userapi.com/c856224/u89830763/docs/d3/eea242e11522/tree_4.png?extra=_6_YwPAbxwy3bEqGsiQblcPewHU8bOmpyPpHz4nPQerJ9fdc1yLfmFSTydSyIOzpn7TS51FvQwxVFgNoAEBMTqnS1dyeFF4xdHKmoR7reIkZYT-0Rs-W7M8UAdNtd37KDK4E5sgZUw_WTiZe7L9k',
+        'https://psv4.userapi.com/c856224/u89830763/docs/d1/4360d2af7153/tree_6.png?extra=qFNBBMthTrt27CrTkDAXdCtfqj0oLMFAjhJLXeVPjxtA6Iq5qJOUZfECDttsmtWib6zcwVotVtU6bTDBxQCB94TAxJy4N48P32Wt0r_lomQAR-DTkLbzntrb_YW3z6wQDKXs0ksRChiUY93379pc',
+        'https://psv4.userapi.com/c856224/u89830763/docs/d11/675e13889e79/tree_7.png?extra=Sf2-yKijYWxHs-5nnOv2KTrMw1IZ-MHBHNZ2-3nRV0aHte4tE15d0qIHsBTUWylKbgmraA82uV5sFaU62V5VmpA1Weth2TEQsVBvhqj8OBeWBgeiJSULtDWvSc-WR2_jzQOvbNkfqeHtXmnUa7bf',
+        'https://psv4.userapi.com/c856224/u89830763/docs/d17/7b6e98204c9a/tree_5.png?extra=g1Ycn9hQzOT-Ufabgn1c-VurVhNmWPE7DWGn2et3BmW7IYczXFhYNat_4c1dcm-LT-tDxZCJDIWGVVYgbdGYi5UnfIdMtiIPGf_EPhbxYyhmRGCkzdC8o0yI5N0b5SzESN0XFWwzGR8q13nA-YGQ',
+    ];
+
     return (
         <div className="feedback_tree_box">
             <div className="feedback_tree_tools">
-                <img style={{marginLeft: "25px"}} src="https://sun1-99.userapi.com/aGKph1Of4NCV7LlmPKqWnOHbYzfycXj9qy3Yww/rwJ5Oqec7Cs.jpg"/>
+                <div className="tree_stage">
+                <img className="tree_img" style={{marginLeft: "25px"}} src={trees[treeStage]}/>
+                <div className="note_big">
+                </div>
+                    <ProgressBar id={note.id} percents={15} setTree={setTreeStage}/>
+                    <div className="note_info">
+                        <h3 className="note_header">{note.header}</h3>
+                        <div className="note_service">
+                            <h3 className="note_func">Progress stat</h3>
+                            <h3 className="note_func">Steps</h3>
+                        </div>
+                        <h3 className="note_deadline">{note.deadline}</h3>
+                    </div>
+                </div>
                 <ul className="feedback_tree_tools_menu">
                     <li onClick={() => handleContent(0)} style={activeContent === 0 ? activeTool : null}>Marks</li>
                     <li onClick={() => handleContent(1)} style={activeContent === 1 ? activeTool : null}>Comments</li>
--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const randomProgress = (id) => {
     const randomize = () => {
@@ -8,22 +8,80 @@ const randomProgress = (id) => {
     //(0,112,60) 0%, rgb(125,194,68)
     let percents = randomize();
     let progress = document.getElementById(id);
-    console.log(progress);
     progress.style.background = !percents ? "transparent" : "rgb(" + (125 - Math.round(125 * percents / 100)) +", " + (194 - Math.round(82 * percents / 100)) + "," + (68 - Math.round(8 * percents / 100)) + ")";
     progress.style.height = 110 + percents + "px";
     let percents_view = document.getElementById(id + '_percents');
     percents_view.innerText = percents + "%";
 };
 
-const ProgressBar = ({id}) => {
+const notRandomProgress = (id, percents) => {
+    let progress = document.getElementById(id);
+    progress.style.background = !percents ? "transparent" : "rgb(" + (125 - Math.round(125 * percents / 100)) +", " + (194 - Math.round(82 * percents / 100)) + "," + (68 - Math.round(8 * percents / 100)) + ")";
+    progress.style.height = 110 + percents + "px";
+    let percents_view = document.getElementById(id + '_percents');
+    percents_view.innerText = percents + "%";
+};
+
+const ProgressBar = ({id, percents = -1, setTree}) => {
+
+    const [percent, setPercent] = useState(percents);
+
+    const handleClick = () => {
+        if (percent === 0) {
+            setTree(0);
+            setPercent(percent + 15);
+            return ;
+        }
+        if (percent === 15) {
+            setPercent(percent + 25);
+            setTree(1);
+            return ;
+        }
+        if (percent === 40) {
+            setPercent(percent + 25);
+            setTree(2);
+            return ;
+        }
+        if (percent === 65) {
+            setPercent(percent + 15);
+            setTree(3);
+            return ;
+        }
+        if (percent === 80) {
+            setPercent(percent + 10);
+            setTree(4);
+            return ;
+        }
+        if (percent === 90) {
+            setPercent(percent + 10);
+            setTree(5);
+            return ;
+        }
+        if (percent === 100) {
+            setPercent(0);
+            setTree(6);
+            return ;
+        }
+    }
+
+
     return (
         <div className="circle-out">
             <div className="progress" id={id}/>
-            <div
+            {setTree ?  <div
                 className="circle-in"
-                onMouseOver={() => randomProgress(id)}>
+                onClick={() => {
+                        notRandomProgress(id, percent);
+                        handleClick();
+                }}>
                 <p id={id + "_percents"}>0%</p>
-            </div>
+            </div> :
+                <div
+                    className="circle-in"
+                    onMouseOver={() => randomProgress(id)}>
+                    <p id={id + "_percents"}>0%</p>
+                </div> }
+
         </div>
     );
 };
