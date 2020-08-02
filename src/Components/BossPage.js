54837893d0e0c423c96d6c79f11c7e5aa56c071a
saxumcordis <victor123952@gmail.com>
2020-08-02 16:10:34 +0300

    add

--- src/Components/BossPage.js
+++ src/Components/BossPage.js
@@ -1,6 +1,32 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import {ProgressBar} from "./ProgressBar";
 
+const workers = [{
+    id: 0,
+    name: "Alexander",
+    photo: "https://photo-ideal.ru/upload/iblock/6b3/foto_na_rezyume_37.jpg",
+},
+    {
+        id: 1,
+        name: "Nikolay",
+        photo: "https://photo-ideal.ru/upload/iblock/66a/foto_na_rezyume_50.jpg",
+    },
+    {
+        id: 2,
+        name: "Vladimir",
+        photo: "https://photo-ideal.ru/upload/iblock/f01/foto_na_rezyume_16.jpg",
+    },
+    {
+        id: 3,
+        name: "Viktoria",
+        photo: "https://photo-ideal.ru/upload/iblock/041/foto_na_rezyume_7.jpg"
+    },
+    {
+        id: 4,
+        name: "Marina",
+        photo: "https://photo-ideal.ru/upload/iblock/f26/foto_na_rezyume_32.jpg"
+    }];
 
 const Worker = ({activeWorker, setActiveWorker, worker}) => {
     const activeWorkerStyle = {
@@ -16,33 +42,8 @@ const Worker = ({activeWorker, setActiveWorker, worker}) => {
     );
 };
 
-const WorkersList = () => {
-    const [activeWorker, setActiveWorker] = useState(null);
+const WorkersList = ({activeWorker, setActiveWorker}) => {
 
-    const workers = [{
-        name: "Alexander",
-        photo: "https://photo-ideal.ru/upload/iblock/6b3/foto_na_rezyume_37.jpg",
-    },
-        {
-            id: 1,
-            name: "Nikolay",
-            photo: "https://photo-ideal.ru/upload/iblock/66a/foto_na_rezyume_50.jpg",
-        },
-        {
-            id: 2,
-            name: "Vladimir",
-            photo: "https://photo-ideal.ru/upload/iblock/f01/foto_na_rezyume_16.jpg",
-        },
-        {
-            id: 3,
-            name: "Viktoria",
-            photo: "https://photo-ideal.ru/upload/iblock/041/foto_na_rezyume_7.jpg"
-        },
-        {
-            id: 4,
-            name: "Marina",
-            photo: "https://photo-ideal.ru/upload/iblock/f26/foto_na_rezyume_32.jpg"
-        }];
     return (
         <div className="workers_list">
             {workers.map(worker => <Worker worker={worker} activeWorker={activeWorker}
@@ -59,14 +60,43 @@ const BossMenu = () => {
             <li>Ожидание фидбека: 1</li>
         </div>
     )
-}
+};
+
+const WorkerFull = ({id}) => {
+    const [currentTasks, setTask] = useState(0);
+    const worker = workers.filter(e => e.id === id)[0];
+    return (<div className="note_full">
+        <img style={{width: "200px", marginLeft: "15px", marginTop: "15px", borderRadius: "5px"}} src={worker.photo}/>
+        <ul>
+            <p style={{marginTop: 0}}>{worker.name}</p>
+            <li>Статистика</li>
+            <li className="note_field" style={{cursor: "pointer"}} onClick={() => setTask(!currentTasks)}>Текущие задания</li>
+        </ul>
+        <div className="note">
+            <ProgressBar id={1}/>
+            <div className="note_info">
+                <h3 className="note_header">Сдача проекта</h3>
+                <div className="note_service">
+                    <h3 className="note_func">Progress stat</h3>
+                    <h3 className="note_func">Steps</h3>
+                    <Link to={"/feedbacktree/" + 1}><h3 className="note_func">FeedbackTree</h3></Link>
+                </div>
+                <h3 className="note_deadline">02.08.20</h3>
+            </div>
+        </div>
+    </div>)
+};
 
 const BossPage = () => {
+    const [activeWorker, setActiveWorker] = useState(null);
     return (
         <div className="bosspage_box">
             <div className="bosspage_left">
-                <WorkersList/>
+                <WorkersList activeWorker={activeWorker} setActiveWorker={setActiveWorker}/>
                 <BossMenu/>
+            </div>
+            <div className="bosspage_right">
+                {activeWorker || activeWorker === 0 ? <WorkerFull id={activeWorker}/> : null}
             </div>
         </div>
     );
